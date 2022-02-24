fd87c41bb6b041a2866093e4a8bce53bcd070b91
moemoemeo <100294395+moemoemeo@users.noreply.github.com>
2022-02-24 17:03:20 +0700

    main

--- Doan2/Assets/code/enemy.cs
+++ Doan2/Assets/code/enemy.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemy : MonoBehaviour
 {
+    public NavMeshAgent scp;
+    public Transform Player;
     public float health = 100f;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +17,7 @@ public class enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        scp.SetDestination(Player.position);
     }
     public void TakeDam(float khoang)
     {
